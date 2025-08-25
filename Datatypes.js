a727dc3920c5e9ad327bc454e0a545bbe1c66ebf
Martin <Okonyemartin199@gmail.com>
2025-08-24 18:45:43 -0700

    Update Datatypes.js

--- Datatypes.js
+++ Datatypes.js
@@ -1,7 +1,13 @@
-let firstname = 'Martin'
-let surname = 'Okonye'
-let favoritecolor = 'Red'
-let Gender = 'Male'
-let Age = 35;
-let bill = '5000000'
-console.log('my name is: ' + firstname + ' ' + surname);
+let firstname= 'Martin'
+let surname ='Okonye'
+let age = 35
+let bill =5000000
+let favouritecolor = 'green'
+let gender ='male'
+let maritalstatus = 'single'
+let nationality = 'Nigerian'
+let religion = 'Christian'
+let occupation = 'beginner mobible app developer'
+let ismarried = 'false'
+let isselfemployed = 'true'
+console.log= 'my name is'(firstnmae + surname + age + bill + favouritecolor + occupation)
